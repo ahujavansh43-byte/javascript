bcb1f3fce0c4640d86a4c71a91f36878318fbd6f
ahujavansh43-byte <ahujavansh43@gmail.com>
2026-01-30 15:36:56 +0530

    feat:async, sync and timer code

--- script.js
+++ script.js
@@ -31,11 +31,11 @@
 // para2.forEach((ele)=>console.log(ele.textContent))
 
 
-const para2=document.querySelectorAll('.para')
-para2[0].innertext="this is now upadated paragraph"
-para2[0].textContent="this is now upadated paragraph"
-para2[0].innerHTML="<div><b>hello world</b></div>"
-para2[1].style.color="green"
+// const para2=document.querySelectorAll('.para')
+// para2[0].innertext="this is now upadated paragraph"
+// para2[0].textContent="this is now upadated paragraph"
+// para2[0].innerHTML="<div><b>hello world</b></div>"
+// para2[1].style.color="green"
 
 
 // const para=document.querySelector('.para')
@@ -59,17 +59,97 @@ para2[1].style.color="green"
 // })
 
 
+// const handleSubmit=(event)=>{
+//     event.preventDefault()
+//     console.log("Form submitted")
+// }
 
-const handleSubmit=(event)=>{
-    event.preventDefault()
-    console.log("Form submitted")
-}
+// const form=document.querySelector('form')
+// form.addEventListener('submit',handleSubmit)
 
-const form=document.querySelector('form')
-form.addEventListener('submit',handleSubmit)
+// const btn=document.querySelector("button")
+// const div=document.querySelector(".container")
+// const div2=document.querySelector(".outer")
+
+// btn.addEventListener("click",function(){ console.log("button")})
+// btn.addEventListener("click",function(){ console.log("Div")})
+// btn.addEventListener("click",function(){ console.log("Div2")})
 
 
+// debugger
+// console.log(a);
+// console.log(b);
+// var a=78;
+// let b=345;
 
 
+// console.log(a);
+// console.log(b);
 
 
+// function print(){
+//     let c=45
+//     console.log(c);
+//     console.log("Inside fn")
+// }
+// print()
+
+// function greet(){
+//     var message="hello user";
+//     console.log(message);
+// }
+// greet();
+
+// console.log(message);
+
+// let total=100;
+
+// function calculator(){
+//     console.log(total);
+//      total=100;
+    
+// }
+// calculator()
+
+
+//< ---------UNIT-2--------->
+
+// window.alert(`are you sure you enter in your dream place that is my heart`);
+// let heart=window.prompt("say something");
+// console.log("Do you love me?");
+// setTimeout(()=>{
+//     console.log("i konw you choose yes")
+// },2000);
+// console.log("YeS/no");
+
+// setTimeout(()=>{
+//     alert(`hello baby`)
+// },3*1000);
+
+// setInterval(()=>{
+//     console.log(`hello janeman`)
+// },2000);
+
+// const timerid=setInterval(()=>{
+//     console.log(`hi`)
+// },1000);
+
+// setTimeout(()=>{
+//     clearInterval(timerid)
+// },10*1000);
+
+// let count=10;
+
+// const id=setInterval(()=>{
+//     if(count==1)clearInterval(id)
+//         console.log(count)
+//     count-=1
+//     // console.log(`hello begum`);
+// },1000);
+
+
+console.log("Do you love me?");
+setTimeout(()=>{
+    console.log("i konw you choose yes")
+},0);
+console.log("YeS/no");
